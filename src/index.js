3be6f433fb9b7189eaf3a21799064d05e84db058
Kamal Patra <kamal.patra369@gmail.com>
2025-02-24 19:19:32 +0530

    Add user and video model

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({ path: "./env" });
+
+connectDB()
+  .then(() => {
+    app.on("ERROR", (err) => {
+      console.log("Error starting server", err);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("Error connecting to DB", err));
